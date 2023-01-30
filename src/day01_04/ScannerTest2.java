33c17f03aa74c71933a45c4b6529b7b04aae9998
harok <gkfhrwkd23@naver.com>
2023-01-30 13:56:44 +0900

    day01~04 강의

--- src/day01_04/ScannerTest2.java
+++ src/day01_04/ScannerTest2.java
@@ -10,7 +10,7 @@ public class ScannerTest2 {
 		System.out.print("나이를 입력하세요 : ");
 		age = sc.nextInt();
 		
-//		문자열을 제외한 데이터를 입력받은 후 nextLine() 메소드가
+//		'문자열을 제외한 데이터'를 입력받은 후 nextLine() 메소드가
 //		실행이 돼야 한다면 키보드 버퍼의 엔터키를 읽는 문제 발생.
 		
 //		키보드 버퍼를 비워주는 동작을 실행 후 nextLine() 메소드 실행
